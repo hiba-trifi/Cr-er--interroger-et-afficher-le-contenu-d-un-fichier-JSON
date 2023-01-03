2769e068da8aafc280c7486d563ceac3d792b560
Hiba Trifi <trifi.hiba.solicode@gmail.com>
2023-01-03 10:55:33 +0000

    hiba

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ var header = document.getElementById('header')
 var modeCheck
 function mode() {
     if (document.getElementById('mySwitch').checked) {
-        modeCheck == false
-        document.getElementById('boody').style.backgroundColor = "rgb(26, 4, 47)"
+        modeCheck == false   
+        header.style.backgroundColor = "#FFC001"
+        header.style.transition = "0.5s"
+        document.getElementById('boody').style.backgroundColor = "#1E0E34"
         document.querySelector('.table').style.borderColor = "white"
         document.getElementById('tbody').style.color = "white"
-        header.style.backgroundColor = "#ffc107"
-        document.querySelector('.fa-solid').style.color = "#ffc107"
-        document.querySelector('.btn').style.backgroundColor = "rgb(26, 4, 47)"
+        document.querySelector('.fa-solid').style.color = "#FFC001"
+        document.querySelector('.btn').style.backgroundColor = "#1E0E34"
+        document.getElementById('name1').style.display = "block"
+        document.getElementById('name2').style.display = "none"
+        document.getElementById('darkMod').style.color = "#1E0E34"
+        document.getElementById('headers').style.color = "#FFC001"
     }
     else {
         modeCheck == true
-        header.style.backgroundColor = "rgb(26, 4, 47)"
+        header.style.backgroundColor = "#1E0E34"
+        header.style.transition = "0.5s"
         document.getElementById('boody').style.backgroundColor = "white"
-        document.querySelector('.table').style.borderColor = "rgb(26, 4, 47)"
-        document.getElementById('tbody').style.color = "rgb(26, 4, 47)"
-        document.querySelector('.fa-solid').style.color = "rgb(26, 4, 47)"
-        document.querySelector('.btn').style.backgroundColor = "#ffc107"
+        document.querySelector('.table').style.borderColor = "#1E0E34"
+        document.getElementById('tbody').style.color = "#1E0E34"
+        document.querySelector('.fa-solid').style.color = "#1E0E34"
+        document.querySelector('.btn').style.backgroundColor = "#FFC001"
+        document.getElementById('name1').style.display = "none"
+        document.getElementById('name2').style.display = "block"
+        document.getElementById('darkMod').style.color = "#FFC001"
+        document.getElementById('headers').style.color = "#FFC001"
     }
 }
 function show() {
@@ -28,13 +38,14 @@ const checkpoint = 180;
 window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
     if (currentScroll < checkpoint) {
-        header.style.transition = "0.5s"
+        header.style.transition = "0.6s"
         header.style.opacity = "1"
     } else {
-        header.style.opacity = "0.6"
+        header.style.opacity = "0.8"
     }
 
 });
+
 
 // XHTTP REQUEST  ////////////////////////////////////
 var xhttp = new XMLHttpRequest();
@@ -56,8 +67,8 @@ xhttp.onreadystatechange = function () {
                 var cell4 = row.insertCell();
                 var cell5 = row.insertCell();
                 var cell6 = row.insertCell();
-                var cell7 = row.insertCell();              
-            }  
+                var cell7 = row.insertCell();
+            }
             // CELLS CONTENT ////////////////////////
             cell1.innerHTML = "<a target='_blank' href=" + data.movies[i].Poster + "><img class='img-fluid' src=" + data.movies[i].Poster + " ></a>"
             cell2.innerHTML = data.movies[i].title;
@@ -75,43 +86,36 @@ xhttp.onreadystatechange = function () {
         var input = document.querySelector('.form-control')
         var table = document.getElementById('table');
         input.addEventListener('input', () => {
-            table.querySelectorAll('tr').forEach(row => row.hidden = row.cells[1].innerText.toUpperCase().indexOf(input.value.toUpperCase()) === -1)
+            table.querySelectorAll('tr').forEach(row => row.hidden = row.cells[0].innerText.toUpperCase().indexOf(input.value.toUpperCase()) === -1)
         })
-///// SORT ////////////////////////////
-th = document.getElementsByTagName('th')
-for(let c = 0 ; c < th.length ; c++){
-  th[c].addEventListener('onClick' , item(c))
-}
-function item(c){
-  return function (){
-    sortTable(c)
-}}
-var rows = table.rows
-function sortTable(c) {
-    // [...rows].sort((a,b)=>{
-    //     let x = a.querySelectorAll('td')[c].textContent.toLowerCase(),
-    //      y = b.querySelectorAll('td')[c].textContent.toLowerCase();
-    //      console.log(x , y)
-    //   return  x < y ? -1 : 1;
-    // }).map(sorted_row => document.querySelectorAll('tbody').appendChild(sorted_row ))
-  var table, rows
-  table = document.getElementById("table");
-  var switching = true;
-  while (switching) {
-    switching = false;
-    rows = table.rows;
-    for ( var z = 1; z < (rows.length - 1); z++) {
-      shouldSwitch = false;
-      var x = rows[z].getElementsByTagName("TD")[c];
-     var y = rows[z + 1].getElementsByTagName("TD")[c];
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        rows[z].parentNode.insertBefore(rows[z + 1], rows[z]);
-        switching = true;
-        break;
-      }
-    }
-  }
-}
+        ///// SORT ////////////////////////////
+        th = document.getElementsByTagName('th')
+        for (let c = 0; c < th.length; c++) {
+            th[c].addEventListener('click', item(c))
+        }
+        function item(c) {
+            return function () {
+                sortTable(c)
+            }
+        }
+        function sortTable(c) {
+            var table = document.getElementById("table");
+            var switching = true;
+            while (switching) {
+                switching = false;
+                var rows = table.rows;
+                for (var z = 1; z < (rows.length - 1); z++) {
+                    shouldSwitch = false;
+                    var x = rows[z].getElementsByTagName("TD")[c];
+                    var y = rows[z + 1].getElementsByTagName("TD")[c];
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        rows[z].parentNode.insertBefore(rows[z + 1], rows[z]);
+                        switching = true;
+                        break;
+                    }
+                }
+            }
+        }
 
 
     }
